e166b99ec83ae23758a2ce23a9664c8aa97f18f1
Victorine-Amani <victorinenyagwala@gmail.com>
2021-03-08 21:14:39 +0300

    Javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+function fruit() {
+    var fruits = ["Avocado", "Grapes", "Mangoes", "Oranges", "Kiwi"];
+    for (var i = 0; i < fruits.length; i++) {
+        console.log(fruits[i])
+    }
+}
+fruit()
