126fe22314fc7e876f7b086e305e86bd89db60ef
Victorine-Amani <victorinenyagwala@gmail.com>
2021-03-16 08:52:45 +0300

    Assignment1

---
+++ text.js
@@ -0,0 +1,7 @@
+function fruits() {
+    var list = ["Avocado", "Grapes", "Mangoes", "Apple", "Oranges", "Kiwi"];
+    for (var i = 0; i < list.length; i++) {
+        console.log(list[i])
+    }
+}
+fruits()
